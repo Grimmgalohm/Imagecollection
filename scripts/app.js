85f9202a19a508db5df2cdb11e89c26d6fc364dd
Cesar Luna <cesarlunalimon@gmail.com>
2018-05-19 22:32:10 -0500

    Ya funciona javascript para abrir y cerrar la galería, solo falta linkear las imagenes

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,17 @@
 window.onload=(function(){
     initializate();
 })();
-function initializate(){
+function initializate (){
     var toshow = document.querySelectorAll(".link");
     var mostrar = document.querySelector(".view");
-    toshow.foreach(link => link.addEventListener("click", function(e){
+    toshow.forEach(HTMLLinkElement=>HTMLLinkElement.addEventListener("click", function(e){
         e.preventDefault();
         mostrar.style.display="flex";
     }));
     var cerrar = document.querySelector(".close");
+    var ocultar = document.querySelector(".view");
     cerrar.addEventListener("click", function(e){
         e.preventDefault();
         ocultar.style.display="none";
     });
-}
+};
