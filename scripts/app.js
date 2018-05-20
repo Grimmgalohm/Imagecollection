d91fd544957306ee772140e5d18e9221e136bcc7
Cesar Luna <cesarlunalimon@gmail.com>
2018-05-19 22:31:19 -0500

    Javascript

--- scripts/app.js
+++ scripts/app.js
@@ -2,20 +2,15 @@ window.onload=(function(){
     initializate();
 })();
 function initializate(){
-    var toshow = document.querySelector(".link");
+    var toshow = document.querySelectorAll(".link");
     var mostrar = document.querySelector(".view");
-    toshow.addEventListener("click", function(e){
+    toshow.foreach(link => link.addEventListener("click", function(e){
         e.preventDefault();
         mostrar.style.display="flex";
-    });
-} 
- /*
-function initializate(){
+    }));
     var cerrar = document.querySelector(".close");
-    var ocultar = document.querySelector(".view");
     cerrar.addEventListener("click", function(e){
         e.preventDefault();
         ocultar.style.display="none";
     });
 }
-*/
