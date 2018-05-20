36f8e9861cca0fb1e597e0d4ec9db5953c217351
Cesar Luna <cesarlunalimon@gmail.com>
2018-05-20 00:49:14 -0500

    Terminado el javascript, funcionando, con problemas en imagenes super grandes

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,19 @@ window.onload=(function(){
     initializate();
 })();
 function initializate (){
-    var toshow = document.querySelectorAll(".link");
-    var mostrar = document.querySelector(".view");
-    toshow.forEach(HTMLLinkElement=>HTMLLinkElement.addEventListener("click", function(e){
+    var images = document.querySelectorAll(".images a"); //selecciona la clase imagenes y los elementos <a
+    var viewer = document.querySelector(".view");// selecciona la clase .view que es el back del visualizador
+    images.forEach(link => link.addEventListener("click", function(e){
         e.preventDefault();
-        mostrar.style.display="flex";
-    }));
+        
+        var img = this.querySelector("img");
+        var viewimg =viewer.querySelector("img");
+        viewimg.src = img.src;
+        viewer.style.display="flex";
+    }))
     var cerrar = document.querySelector(".close");
-    var ocultar = document.querySelector(".view");
     cerrar.addEventListener("click", function(e){
         e.preventDefault();
-        ocultar.style.display="none";
-    });
-};
+        viewer.style.display="none";
+});
+}
